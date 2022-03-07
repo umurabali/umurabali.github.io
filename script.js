2fe41462c5d20596bc2e3043e2b7612ee2e12f76
umurabali <umurabali@gmail.com>
2022-03-07 14:48:28 +0300

    add board

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let guessCount = 6;
 let currentGuess = [];
 let nextLetter = 0;
 let rightGuessString = 'KULAK';
+
+function initBoard() {
+    let board = document.getElementById("game-container");
+
+    for (let i = 0; i < guessCount; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+        
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+initBoard()
